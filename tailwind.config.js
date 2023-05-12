077aa7d806a3de1fa87d6db1e25df2231a85cbf7
Mario Campbell <hector.campbell@mercadolibre.com>
2023-05-11 21:38:26 -0300

    feat: brand ember config in tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ['./app/**/*.{gjs,gts,hbs,html,js,ts}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        ember: '#BD3122',
+        'burnt-ember': '#9B2918',
+        blue: '#1E719B',
+        'light-blue': '#74B0CE',
+        yellow: '#FBC840',
+        'ember-black': '#212121',
+        'ember-gray': '#817F7F',
+        'faint-gray': '#EFEBEA',
+        linen: '#FDF7F6',
+        'ember-white': '#FDFDFD',
+      },
+    },
   },
   plugins: [],
 };
