19f041d823b8ab70d1892cff0c0d2f684ee3f798
Mario Campbell <hector.campbell@mercadolibre.com>
2023-05-26 02:03:23 -0300

    feat: upload images files in drawer component

--- app/components/drawer/index.js
+++ app/components/drawer/index.js
@@ -5,14 +5,14 @@ import { service } from '@ember/service';
 
 export default class DrawerComponent extends Component {
   @service drawer;
+
   @tracked editAvatar = false;
+  @tracked showModalDelete = false;
+  @tracked imgName = this.args.title || 'avatar.svg';
+  @tracked imgSrc = this.args.image || '/images/mascots/avatar.svg';
+  @tracked imgFile;
 
-  @action
-  closeDrawer() {
-    console.log('closeDrawer');
-    this.drawer.close();
-  }
-
+  // avatar hover
   @action
   mouseEnter() {
     this.editAvatar = true;
@@ -21,5 +21,51 @@ export default class DrawerComponent extends Component {
   @action
   mouseLeave() {
     this.editAvatar = false;
+  }
+
+  // add mascot
+  @action
+  addMascot() {
+    console.log('mascota agregada correctamente');
+    this.drawer.toggle();
+  }
+
+  // delete mascot
+  @action
+  deleteMascot(id) {
+    console.log('mascota eliminada', id);
+    this.showModalDelete = false;
+    this.drawer.toggle();
+  }
+
+  // open modal delete
+  @action
+  openModalDelete() {
+    this.showModalDelete = true;
+  }
+
+  // close modal delete
+  @action
+  closeModalDelete() {
+    this.showModalDelete = false;
+  }
+
+  // get image src
+  get imageSource() {
+    return this.imgSrc;
+  }
+
+  // selected image attributes
+  @action
+  selectedImage(event) {
+    this.imgName = event.target.files[0].name;
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imgSrc = reader.result;
+      this.imgFile = reader.result;
+    };
+
+    reader.readAsDataURL(event.target.files[0]);
   }
 }
